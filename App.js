93cd0b7bfd25c971d302c392ed1ccd62cc829da8
Vijay <vijaysethu0101@gmail.com>
2024-07-03 19:54:28 +0530

    feat:lay of foundation

--- App.js
+++ App.js
@@ -1,2 +1,26 @@
-import { React} from "react";
-import { ReactDOM } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const Elements=()=>(
+    <h1>Dev Role</h1>
+
+)
+
+const Heading = () => {
+    return (
+        <div>
+        <Elements/>
+        {Elements()}
+            <h1>
+                Hello Developer Vijay
+            </h1>
+        </div>
+    );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Heading />
+);
