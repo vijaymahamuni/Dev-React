29019a1f9ed32e0366513e6f26717150be461207
Vijay <vijaysethu0101@gmail.com>
2024-07-11 23:34:21 +0530

    feat:Adding api data and UI design

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,28 @@
 import RestroCard from "./RestroCard";
-import { MOCKDATA } from "../utils/mockdata";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-    const [listofRes, setListofRes] = useState(MOCKDATA)
+    const [listofRes, setListofRes] = useState([])
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.89960&lng=80.22090&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json)
+        console.log("format type", json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        setListofRes(json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+    if(listofRes.length===0){
+        return(
+            <Shimmer />
+
+        )
+    }
+
+
     const TopRatedfunc = () => {
         const TopRatedfilter = listofRes.filter((resData) => resData.info.avgRating > 4)
         setListofRes(TopRatedfilter)
@@ -13,14 +31,12 @@ const Body = () => {
         <div className="body-container">
             <button className="toprated-btn" onClick={TopRatedfunc}>TopRated Restro</button>
             <div className="res-container">
-
                 {listofRes.map((item) => (
-                    <RestroCard key={item.id} resData={item} />
-
+                    <RestroCard key={item.info.id}resData={item} />
                 ))}
-
             </div>
         </div>
+
 
     )
 }
--- src/components/RestroCard.js
+++ src/components/RestroCard.js
@@ -1,6 +1,5 @@
 import { RESTRO_IMG } from "../utils/constants";
 const RestroCard=({resData})=>{
-    console.log("resData",resData)
     return(
         <div className="restau-container">
             {/* <div className="search-box">
@@ -9,10 +8,14 @@ const RestroCard=({resData})=>{
             <div className="restau-card">
                 <img className="restau-img" src={RESTRO_IMG+resData.info.cloudinaryImageId}/>
                 <p style={{fontSize:"18px",fontWeight:"bold"}}>{resData.info.name}</p>
-                <p style={{fontSize:"16px",fontWeight:"bold"}}>{resData.info.avgRating}</p>
+                <p style={{fontSize:"16px",fontWeight:"bold"}}>{resData.info.cuisines.join(' ,')}</p>
+                <p style={{fontSize:"16px",fontWeight:"bold"}}>{resData.info.avgRating} stars</p>
+                <p style={{fontSize:"16px",fontWeight:"bold"}}>{resData.info.description}</p>
+                <p style={{fontSize:"16px",fontWeight:"bold"}}>{resData.info.costForTwo}</p>
+
                 <p style={{fontSize:"16px",fontWeight:"bold"}}>{resData.info.sla.slaString}</p>
-                <p style={{fontSize:"16px"}}>{resData.info.cuisines.join(' ,')}</p>
-                <p style={{fontSize:"16px"}}>{resData.info.locality}</p>
+
+
 
                
             </div>
