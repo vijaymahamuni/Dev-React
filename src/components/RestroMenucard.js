f10a050b43a5c870a7ef60e93b800ba78af7f066
Vijay <vijaysethu0101@gmail.com>
2024-08-01 00:21:19 +0530

    feat:Update data Layer

--- src/components/RestroMenucard.js
+++ src/components/RestroMenucard.js
@@ -2,19 +2,21 @@ import Shimmer from './Shimmer';
 import { useParams } from "react-router-dom";
 import { MENUCARD_IMG } from "../utils/constants";
 import useRestaurantsMenu from "../utils/useRestaurantsMenu";
+import RestaurantCategory from './RestaurantCategory';
 
 const RestroMenucard = () => {
     const { resId } = useParams();
     const resInfo = useRestaurantsMenu(resId);
 
     const name = resInfo?.cards[2]?.card?.card?.info.name;
-    const avgRating = resInfo?.cards[2]?.card?.card?.info.avgRating;
-    const totalRatingsString = resInfo?.cards[2]?.card?.card?.info.totalRatingsString;
-    const areaName = resInfo?.cards[2]?.card?.card?.info.areaName;
-    const expectationNotifiers = resInfo?.cards[2]?.card?.card?.info.expectationNotifiers[0].text;
     const cuisines = resInfo?.cards[2]?.card?.card?.info.cuisines;
     const costForTwoMessage = resInfo?.cards[2]?.card?.card?.info.costForTwoMessage;
-    const itemCards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
+    // const itemCards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards;
+
+    const Categories= resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter((c) =>
+        c.card?.card?.["@type"] ==="type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+    console.log("category",Categories)
 
     if (resInfo === null) {
         return (
@@ -23,44 +25,23 @@ const RestroMenucard = () => {
     }
 
     return (
-        <div className="restromenu">
-            <h1>{name}</h1>
-            <div className="header-menu">
-                <h5>{avgRating}({totalRatingsString}) -{costForTwoMessage}</h5>
-                <p>{cuisines.join(', ')} </p>
-                <p>{areaName}</p>
-                <p>{expectationNotifiers}</p>
+        <div className="text-center m-4">
+            <h1 className='font-bold text-xl'>{name}</h1>
+            <div className="mt-4 font-bold">
+                <h5>{cuisines.join(', ')}  -{costForTwoMessage}</h5>
+       
 
             </div>
-            <h2>Menu</h2>
-            {itemCards && itemCards.length > 0 ? (
-                <ul>
-                    {itemCards.map(item =>
-                        <li key={item.card.info.id} className="listMenu" >
-                            <h3>{item.card.info.name}</h3>
-                            <p style={{ fontWeight: 'bold' }}>
-                                Rs.{item.card.info.price / 100}
-                            </p>
-                            <p style={{ color: "green", fontWeight: "bold" }}>
-                                {item.card.info.ratings.aggregatedRating.rating}({item.card.info.ratings.aggregatedRating.ratingCountV2})
-                            </p>
-                            <p style={{ color: "gray" }}>
-                                {item.card.info.description}
-                            </p>
-                            <div className="menucard-imgdiv">
-                                <img src={MENUCARD_IMG + item.card.info.imageId} className="menucard-img" />
+            <div>
+                {Categories.map((item)=>
+                (
+                    <RestaurantCategory />
 
-                            </div>
-
-
-                        </li>
-                    )}
-                </ul>
-
-            ) : (
-                <p>No Menu cards here</p>
-            )}
-
+                )
+                
+                )}
+            </div>
+           
         </div>
 
     )
